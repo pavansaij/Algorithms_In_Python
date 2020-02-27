a56980c11cd9a6daf21b60073596d106531518c3
Prasad Jandhyala <40313552+pavansaij@users.noreply.github.com>
2020-02-27 23:14:22 +0530

    Update Weighted_Quick_Union.py

--- Weighted_Quick_Union.py
+++ Weighted_Quick_Union.py
@@ -1,35 +1,28 @@
-class WeightedUnion:
-    def __init__(self,size):
-        self.id = [i for i in range(size)]
-        self.wg = [i for i in range(1,size+1)]
-        self.rt = [i for i in range(size)]
-    
-    def union(self,node1,node2):
-        root1 = self.root(node1)
-        root2 = self.root(node2)
-        if root1 == root2:
+class UnionFind:
+    def __init__(self, n):
+        self._id = list(range(n))
+        self._sz = [1] * n
+        self.cc = n  # connected components
+
+    def _root(self, i):
+        j = i
+        while (j != self._id[j]):
+            self._id[j] = self._id[self._id[j]]
+            j = self._id[j]
+        return j
+
+    def find(self, p, q):
+        return self._root(p) == self._root(q)
+
+    def union(self, p, q):
+        i = self._root(p)
+        j = self._root(q)
+        if i == j:
             return
-
-        if self.wg[node2] > self.wg[node1]:
-            self.id[node1] = root2
-            self.rt[node1] = root2
-            self.wg[root2]+= self.wg[node1]
-            self.wg[node1] = self.wg[node2] = self.wg[root2]
+        if (self._sz[i] < self._sz[j]):
+            self._id[i] = j
+            self._sz[j] += self._sz[i]
         else:
-            self.id[node2] = root1
-            self.rt[node2] = root1
-            self.wg[root1]+= self.wg[node2]
-            self.wg[node1] = self.wg[node2] = self.wg[root1]
-
-    def root(self,parent):
-        while (parent != self.id[parent]):
-            parent = self.id[parent]
-        return parent
-
-n, m = map(int, input().split(" "))
-tree = WeightedUnion(n)
-for _ in range(m):
-    x, y = map(int, input().split(" "))
-    tree.union(x-1,y-1)
-print(tree.id)
-print(tree.rt)
+            self._id[j] = i
+            self._sz[i] += self._sz[j]
+        self.cc -= 1
